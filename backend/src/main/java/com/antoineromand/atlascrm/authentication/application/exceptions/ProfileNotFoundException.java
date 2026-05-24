69760f3cc12313962ee6314cbe57c090bed65232
Antoine Romand <antoine.romand@epitech.eu>
2026-05-24 20:04:55 +0200

    fix: remove deprecated profile package

--- backend/src/main/java/com/antoineromand/atlascrm/authentication/application/exceptions/ProfileNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.antoineromand.atlascrm.authentication.application.exceptions;
-
-public class ProfileNotFoundException extends AuthenticationException {
-  public ProfileNotFoundException() {
-    super("PROFILE_NOT_FOUND", "Profile not found.");
-  }
-}
--- backend/src/main/java/com/antoineromand/atlascrm/authentication/application/usecase/profile/IGetProfileUseCase.java
+++
@@ -1,8 +0,0 @@
-package com.antoineromand.atlascrm.authentication.application.usecase.profile;
-
-import com.antoineromand.atlascrm.authentication.domain.Profile;
-import java.util.UUID;
-
-public interface IGetProfileUseCase {
-  Profile execute(UUID credentialsId);
-}
